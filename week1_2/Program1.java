fc1e58473ccc8d8bd4b07d5d98c503608404fc33
ks8891-blip <ks8891@srmist.edu.in>
2026-03-03 14:16:57 +0530

    Level1

--- week1_2/Program1.java
+++ week1_2/Program1.java
@@ -1,14 +1,8 @@
-import java.util.*;
-public class Pr1 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        float celsius;
-        System.out.println("Enter Temperature:");
-        celsius=sc.nextFloat();
-        float fah;
-        fah=(celsius*(9/5f))+32;
-        System.out.println(String.format("The %.2f celsius is %.2f fahrenheit",celsius,fah));
-        sc.close();
+public class Practice1{
+    public static void main(String[]args){
+        int dob=2000;
+        int present=2024;
+        System.out.println("Hary's age in "+present+" is "+(present-dob));
+
     }
-    
 }
