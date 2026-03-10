37945ea16f9755d44d8d4418af990a7475f7cb09
ks8891-blip <ks8891@srmist.edu.in>
2026-03-10 12:48:12 +0530

    level1

---
+++ week1_2/program8.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Practice8 {
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        double km;
+        km=sc.nextDouble();
+        double mile=(km*1.6);
+        System.out.println(String.format("The total miles is %.3f mile fot the give %.3f km",mile,km));
+        sc.close();
+
+    }
+    
+}
