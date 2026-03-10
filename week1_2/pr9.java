ff167de4e2670112c2e31007bb7a49fa8f0f568d
ks8891-blip <ks8891@srmist.edu.in>
2026-03-10 22:28:44 +0530

    level3

---
+++ week1_2/pr9.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Pr9 {
+    public static void main(String[]args){
+        Scanner sc =new Scanner(System.in);
+        float pounds,kilo;
+        System.out.println("Enter weight in Pounds:");
+        pounds=sc.nextFloat();
+        kilo=(pounds/2.2f);
+        System.out.println(String.format("The weight of the person in pounds is %.2f and in kg is %.2f",pounds,kilo));
+        sc.close();
+    }
+    
+}
