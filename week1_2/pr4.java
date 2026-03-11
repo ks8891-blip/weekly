ff5023412601e28b79e5cdfc6683569107029e98
KETHA DEVI SRI SIVA CHARAN <ks8891@srmist.edu.in>
2026-03-11 14:01:59 +0530

    Update pr4.java

--- week1_2/pr4.java
+++ week1_2/pr4.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Pr4 {
+public class pr4 {
     public static void main(String[]args){
         Scanner sc =new Scanner(System.in);
         String name,fromCity,viaCity,toCity;
