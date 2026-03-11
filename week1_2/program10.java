f2ca8b496e8b1f169d8f8f91280ea3d0435f280b
KETHA DEVI SRI SIVA CHARAN <ks8891@srmist.edu.in>
2026-03-11 13:59:26 +0530

    Update program10.java

--- week1_2/program10.java
+++ week1_2/program10.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Practice10 {
+public class program10 {
     @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
